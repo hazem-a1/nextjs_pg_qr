488e830fb7554a1c5d8aeabc9737f5ffc9cb56e3
hazem <53303934+hazem-a1@users.noreply.github.com>
2024-08-05 16:47:00 +0300

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "@/providers/SessionProvider";
+import { Suspense } from "react";
+import GoogleAnalytics from "@/components/google-analytics/google-analytics";
+import CookieBanner from "@/components/CookieBanner/cookie-banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +20,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><SessionProvider>{children}</SessionProvider></body>
+      <Suspense fallback={null}>
+        <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GTM_ID || 'G-6W04137CJB'} />
+      </Suspense>
+      <body className={inter.className}>
+        <CookieBanner />
+        <SessionProvider>{children}</SessionProvider></body>
     </html>
   );
 }
